650b2ae195d5d36d55c7e081ff37d24c7503e05d
dealmacenes <dealmacenes@gmail.com>
2026-05-12 13:48:21 +0000

    Remove vite-plugin-prerender from dependencies and configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import prerender from 'vite-plugin-prerender';          
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), prerender({
-    routes: ['/'],
-  })],
+  plugins: [react(), tailwindcss()],
   server: {
     host: true,
     port: 1990,
