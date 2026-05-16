3663156565894c7371057245ce7997fa12cdb1e9
dealmacenes <dealmacenes@gmail.com>
2026-05-16 06:40:49 -0300

    Adjust styling for WhatsApp div and share button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -775,7 +775,7 @@ function TroceoSection({ onSelectCut }) {
           </div>
         </div>
         
-        <div className="border-1 border-l-4 border-green-700 flex items-center gap-4 w-full rounded-r-md bg-green-100 mt-15 p-4">
+        <div className="border-1 border-l-4 border-green-700 flex items-center gap-4 w-full rounded-r-md bg-green-100 my-15 p-4">
                 <FaWhatsapp className="size-11 shrink-0 p-1.5 rounded-full text-green-500 bg-white" />
                 <p className="text-gray-700 font-semibold text-sm text-start sm:mx-auto">
                   Tocá el tipo de troceo que quieras consultar por disponibilidad y
@@ -1401,7 +1401,7 @@ const handleShare = async () => {
           
           <button
             onClick={handleShare}
-            className="fixed size-12 bottom-6 right-6 z-40 bg-white text-main-red shadow-lg rounded-full border border-gray-200 hover:scale-105 active:scale-95 transition-all"
+            className="fixed size-13 bottom-6 right-6 z-40 bg-white text-main-red shadow-lg rounded-full border border-gray-200 hover:scale-105 active:scale-95 transition-all"
             aria-label="Compartir"
           >
               <Share2 className="size-7 shrink-0 justify-self-center" />
