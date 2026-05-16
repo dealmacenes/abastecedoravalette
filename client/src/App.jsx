03fd73ba44e6d77af1539ca987d610c2c94a09c9
dealmacenes <dealmacenes@gmail.com>
2026-05-16 05:35:31 -0300

    Change background color of contact info paragraph

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1087,7 +1087,7 @@ export default function App() {
     Contactanos por WhatsApp <FaWhatsapp size={16} />
   </button>
 
-  <p className="flex text-center gap-2 bg-neutral-600/25 text-white/85 text-[11px] px-4 py-2 rounded-b-xl border-t border-white/20">
+  <p className="flex text-center gap-2 bg-main/30 text-white/85 text-[11px] px-4 py-2 rounded-b-xl border-t border-white/20">
    <ArrowUpRight className="size-5 shrink-0 my-auto"/> Abrí el menu para ver nuestros contactos mayoristas, minoristas y sucursales.
 </p>
 </div>
